b14fb8b95a5b363a0db8168253d0c2a770f12e82
Hoang Duong Nam <hoangduongnampb.2k6@gmail.com>
2026-06-24 16:01:35 +0700

    De thi cuoi mon Python

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Student:
         self.assignment_score = assignment_score
         self.average_score = 0.0
         self.academic_type = ""
+        # Gọi hai hàm tình điểm trung bình và phân loại mỗi khi tạo. Lúc tạo sẽ tự động cập nhật avg và rank
         self.calculate_average()
         self.classify_academic()
         
@@ -23,7 +24,7 @@ class Student:
             self.academic_type = "Khá"
         else:
             self.academic_type = "Giỏi"
-            
+        
     def update_score (self, theory_score, practice_score, assignment_score):
         self.theory_score = theory_score
         self.practice_score = practice_score
@@ -36,18 +37,22 @@ class StudentManager:
     def __init__(self):
         self.students = []
         
+    # Hàm thêm sinh viên
     def add_students (self, student):
         self.students.append(student)
         
+    # Hàm in Header
     def print_table_header (self):
-
         print ("-"*len(header))
         print (header)
         print ("-"*len(header))
-        
+       
+    # Hàm in mỗi sinh viên được truyền vào    
     def print_student_row (self, student):
         print (f"{student.id:<13} | {student.name:<18} | {student.theory_score:<15.2f} | {student.practice_score:<15.2f} | {student.assignment_score:<15.2f} | {student.average_score:<15.2f} | {student.academic_type:<10}")
         print ("-"*len(header))
+        
+    # Hàm in tất cả trong self.students
     def show_all(self):
         if not self.students:
             print ("Không có sinh viên nào trong danh sách")
@@ -55,7 +60,7 @@ class StudentManager:
         self.print_table_header()
         for s in self.students:
             self.print_student_row(s)
-    
+    #Hàm cập nhật điểm của sinh viên
     def update_student (self, id, theory_score, practice_score, assignment_score):
         for s in self.students:
             if id == s.id:
@@ -63,6 +68,7 @@ class StudentManager:
                 return True
         return False
     
+    # Hàm dùng để xóa sinh viên
     def delete_student (self, id):
         for s in self.students:
             if id == s.id:
@@ -70,6 +76,7 @@ class StudentManager:
                 return True
         return False
 
+    # Hàm tìm kiếm sinh viên theo tên, đảm bảo tính chất gần đúng, lấy được nhiều phần tử khi dùng in và đều ép cả hai vế so sánh thành lower
     def search_student (self, name):
         results = [s for s in self.students if name.lower() in s.name.lower()]
         if not results:
@@ -79,12 +86,16 @@ class StudentManager:
         for s in results:
             self.print_student_row(s)
             
+            
+    # Hàm bổ trợ để kiểm tra id có tồn tại hay không
     def is_id_exists (self, id):
         for s in self.students:
             if id == s.id.upper():
                 return True
         return False
     
+    
+# Hàm bổ trợ để kiểm tra có để trống thông tin hay không
 def get_non_empty_string (prompt):
     while True:
         value = input (prompt)
@@ -92,7 +103,7 @@ def get_non_empty_string (prompt):
             print ("Không được để trống")
             continue
         return value
-        
+# Hàm bổ trợ để lấy điểm chuẩn, đảm bảo từ 0 -> 10 và là số thực
 def get_valid_score (prompt):
     while True:
         try:
@@ -104,6 +115,7 @@ def get_valid_score (prompt):
             print ("Định dạng số không hợp lệ")            
 
 def main ():
+    # Khởi tạo các mẫu để test
     Manager = StudentManager()
     Manager.add_students(Student("PTIT001", "Nguyễn Văn A", 5.0, 5.0, 5.0))
     Manager.add_students(Student("PTIT002", "Hoàng Dương B", 9.0, 9.0, 9.0)) 
